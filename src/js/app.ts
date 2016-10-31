c3f32f3791aec352378c20bbe4d983f1beb544fb
frontainer <frontainer@gmail.com>
2016-11-01 08:05:23 +0900

    distでなく元ファイルを参照するように

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,5 +1,5 @@
 import 'core-js/shim';
-import 'zone.js/dist/zone';
+import 'zone.js';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
