2c607c9f71ceeeb91ca914913166a92321881f39
frontainer <frontainer@gmail.com>
2017-01-05 08:29:22 +0900

    ファイルを移動

--- src/js/app-aot.ts
+++
@@ -1,8 +0,0 @@
-import './vendor';
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from '../aot-compiled/src/js/app/app.module.ngfactory';
-if (process.env.NODE_ENV) {
-  enableProdMode();
-}
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
--- src/js/app.ts
+++
@@ -1,8 +0,0 @@
-import './vendor';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
-if (process.env.NODE_ENV) {
-  enableProdMode();
-}
-platformBrowserDynamic().bootstrapModule(AppModule);
