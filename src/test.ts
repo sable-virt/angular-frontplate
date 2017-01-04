4df9d43a6b5bf3a04158cec7a2e030f84374b10b
frontainer <frontainer@gmail.com>
2017-01-05 08:28:50 +0900

    ディレクトリ構成をangular-cliに寄せ

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 import './vendor.ts';
-
+//
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
@@ -14,19 +14,22 @@ import {
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
-declare var require: any;
+interface NodeRequire {
+  context: any;
+}
+declare var require: NodeRequire;
 
 // Prevent Karma from running prematurely.
 __karma__.loaded = function () {};
 
-// // First, initialize the Angular testing environment.
-// getTestBed().initTestEnvironment(
-//   BrowserDynamicTestingModule,
-//   platformBrowserDynamicTesting()
-// );
-// // // Then we find all the tests.
-// let context = require.context('./', true, /\.spec\.ts/);
-// // And load the modules.
-// context.keys().map(context);
-// Finally, start Karma to run the tests.
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+// // Then we find all the tests.
+let context = require.context('./', true, /\.spec\.ts/);
+// And load the modules.
+context.keys().map(context);
+// // Finally, start Karma to run the tests.
 __karma__.start();
